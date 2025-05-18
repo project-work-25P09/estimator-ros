013136833a9c4946eff4d5e7f108186e7979c38d
htoik <henrik.toikka@aalto.fi>
2025-05-18 17:21:37 +0300

    calibration script init

--- scripts/calibrate_optical.py
+++ scripts/calibrate_optical.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+import numpy as np
+
+def read_csv(fp):
+    with open(fp, "r") as f:
+        return [[float(y) for y in x.split(',') if y] for x in f.read().split('\n') if x]
+
+x_fp = './data/calibration/x.csv'
+y_fp = './data/calibration/y.csv'
+
+x = np.array([a[1:3] for a in read_csv(x_fp)])
+y = np.array([a[1:3] for a in read_csv(y_fp)])
+
+x_dist = np.hypot(np.sum(x[:0]), np.sum(x[:1]))
+y_dist = np.hypot(np.sum(y[:0]), np.sum(y[:1]))
+
+print(x_dist, y_dist)
