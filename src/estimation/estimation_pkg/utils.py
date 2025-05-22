d1c97247df1cb91dd8a58c34bf14f4d3056e3455
htoik <henrik.toikka@aalto.fi>
2025-05-22 11:00:54 +0300

    estimator separate from ekf

--- src/estimation/estimation_pkg/utils.py
+++ src/estimation/estimation_pkg/utils.py
@@ -1,5 +1,10 @@
 import numpy as np
 import math
+from estimation_pkg.simple_ekf import InertialEKF
+
+
+def get_ekf():
+    return InertialEKF(1.0 / 200.0, np.eye(15), np.eye(3), np.eye(2), [0.0, 0.0, 0.0])
 
 
 def quaternion_normalize(q):
@@ -10,45 +15,47 @@ def quaternion_normalize(q):
 def quaternion_multiply(q1, q2):
     x1, y1, z1, w1 = q1
     x2, y2, z2, w2 = q2
-    w = w1*w2 - x1*x2 - y1*y2 - z1*z2
-    x = w1*x2 + x1*w2 + y1*z2 - z1*y2
-    y = w1*y2 - x1*z2 + y1*w2 + z1*x2
-    z = w1*z2 + x1*y2 - y1*x2 + z1*w2
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+    y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+    z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
     return np.array([x, y, z, w])
 
 
 def quaternion_to_rotation_matrix(q):
     x, y, z, w = q
     # normalize to avoid numerical drift
-    n = x*x + y*y + z*z + w*w
+    n = x * x + y * y + z * z + w * w
     if n < np.finfo(float).eps:
         return np.eye(3)
     s = 2.0 / n
-    wx, wy, wz = s*w*x, s*w*y, s*w*z
-    xx, xy, xz = s*x*x, s*x*y, s*x*z
-    yy, yz, zz = s*y*y, s*y*z, s*z*z
-    R = np.array([
-        [1 - (yy + zz),     xy - wz,          xz + wy],
-        [xy + wz,           1 - (xx + zz),    yz - wx],
-        [xz - wy,           yz + wx,          1 - (xx + yy)]
-    ])
+    wx, wy, wz = s * w * x, s * w * y, s * w * z
+    xx, xy, xz = s * x * x, s * x * y, s * x * z
+    yy, yz, zz = s * y * y, s * y * z, s * z * z
+    R = np.array(
+        [
+            [1 - (yy + zz), xy - wz, xz + wy],
+            [xy + wz, 1 - (xx + zz), yz - wx],
+            [xz - wy, yz + wx, 1 - (xx + yy)],
+        ]
+    )
     return R
 
 
 def quaternion_to_euler(q):
     x, y, z, w = q
     # roll (x-axis rotation)
-    sinr_cosp = 2.0 * (w*x + y*z)
-    cosr_cosp = 1.0 - 2.0 * (x*x + y*y)
+    sinr_cosp = 2.0 * (w * x + y * z)
+    cosr_cosp = 1.0 - 2.0 * (x * x + y * y)
     roll = math.atan2(sinr_cosp, cosr_cosp)
     # pitch (y-axis rotation)
-    sinp = 2.0 * (w*y - z*x)
+    sinp = 2.0 * (w * y - z * x)
     if abs(sinp) >= 1:
-        pitch = math.copysign(math.pi/2, sinp)
+        pitch = math.copysign(math.pi / 2, sinp)
     else:
         pitch = math.asin(sinp)
     # yaw (z-axis rotation)
-    siny_cosp = 2.0 * (w*z + x*y)
-    cosy_cosp = 1.0 - 2.0 * (y*y + z*z)
+    siny_cosp = 2.0 * (w * z + x * y)
+    cosy_cosp = 1.0 - 2.0 * (y * y + z * z)
     yaw = math.atan2(siny_cosp, cosy_cosp)
     return roll, pitch, yaw
--- src/estimation/scripts/simple_estimation.py
+++ src/estimation/scripts/simple_estimation.py
@@ -5,11 +5,7 @@ from geometry_msgs.msg import Point
 from sensor_msgs.msg import Imu, MagneticField
 from estimation.msg import Estimation, Measurements
 import numpy as np
-import math
-import os
-import time
-import yaml
-from estimation_pkg.ekf import InertialEKF
+from estimation_pkg.ekf import EKF
 import estimation_pkg.utils as utils
 
 
@@ -17,36 +13,15 @@ class EstimatorNode(Node):
     def __init__(self):
         super().__init__("estimator_node")
 
+        self.ekf = utils.get_ekf()
+
+        self.measurements = Measurements()
+        self.publisher = self.create_publisher(Estimation, "/estimation", 10)
+
         self.create_subscription(Point, "/optical", self.optical_callback, 10)
         self.create_subscription(Imu, "/imu/data", self.imu_callback, 200)
         self.create_subscription(MagneticField, "/imu/mag", self.imu_mag_callback, 200)
 
-        self.est_pub = self.create_publisher(Estimation, "/estimation", 10)
-        self.measurements = Measurements
-
-        self.opt_int_x = 0.0
-        self.opt_int_y = 0.0
-        self.latest_imu = None
-
-        self.config_calibration_fp = "./config/calibration.yml"
-        self.config_estimation_fp = "./config/est_params.yml"
-
-        with open(self.config_calibration_fp, "r") as f:
-            calib = yaml.safe_load(f)
-        opt_cfg = calib.get("optical", {})
-        self.optical_x_to_m = opt_cfg.get("x_to_m", 1.0)
-        self.optical_y_to_m = opt_cfg.get("y_to_m", 1.0)
-
-        with open(self.config_estimation_fp, "r") as f:
-            est_cfg = yaml.safe_load(f)
-
-        dt = est_cfg.get("dt", 1.0 / 200.0)
-        Q = np.array(est_cfg.get("Q", np.eye(15).tolist()))
-        R_imu = np.array(est_cfg.get("R_imu", np.eye(3).tolist()))
-        R_opt = np.array(est_cfg.get("R_opt", np.eye(2).tolist()))
-        m_ref = np.array(est_cfg.get("m_ref", [0.0, 0.0, 0.0]))
-
-        self.ekf = InertialEKF(dt, Q, R_imu, R_opt, m_ref)
         self.get_logger().info("EstimatorNode started.")
 
     def imu_callback(self, imu_msg: Imu):
@@ -91,11 +66,7 @@ class EstimatorNode(Node):
     def update_imu(self, acceleration, angular_velocity, orientation):
         self.measurements.acceleration = acceleration
         self.measurements.angular_velocity = angular_velocity
-        (
-            self.measurements.imu_est_yaw,
-            self.measurements.imu_est_pitch,
-            self.measurements.imu_est_roll,
-        ) = utils.quaternion_to_euler(orientation)
+        self.measurements.est_orientation = orientation
 
     def update_magnetic_field(self, magnetic_field):
         self.measurements.magnetic_field = magnetic_field
@@ -109,7 +80,7 @@ class EstimatorNode(Node):
         est = self.ekf.get_estimation_msg()
         est.stamp = self.get_clock().now().to_msg()
         est.measurements = self.measurements
-        self.est_pub.publish(est)
+        self.publisher.publish(est)
 
 
 def main(args=None):
